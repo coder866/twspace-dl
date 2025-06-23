e46a7af863818b61a85430a787a699ec4a835f06
coder866 <coder866@gmail.com>
2025-06-23 05:16:16 +0300

    Minor adjustments on filename

--- twspace_dl/twspace_dl.py
+++ twspace_dl/twspace_dl.py
@@ -12,9 +12,8 @@ from mutagen.mp4 import MP4, MP4Cover
 from .api import API
 from .twspace import Twspace
 
-DEFAULT_FNAME_FORMAT = "(%(creator_name)s)%(title)s-%(id)s"
-MP4_COVER_FORMAT_MAP = {
-    "jpg": MP4Cover.FORMAT_JPEG, "png": MP4Cover.FORMAT_PNG}
+DEFAULT_FNAME_FORMAT = "(%(creator_name)s)_%(title)s_%(id)s"
+MP4_COVER_FORMAT_MAP = {"jpg": MP4Cover.FORMAT_JPEG, "png": MP4Cover.FORMAT_PNG}
 
 
 class TwspaceDL:
@@ -31,11 +30,12 @@ class TwspaceDL:
         filename = self.space.format(self.format_str)
 
         # Keep alphanumeric characters and spaces
-        sanitized_title = re.sub(r'[^\w\s]', '', filename)
+        sanitized_title = re.sub(r"[^\w\s]", "", filename)
         sanitized_title = sanitized_title.replace(
-            ' ', '_')  # Replace spaces with underscores
-        clean_filename = sanitized_title.replace('__', '_').replace('.', '')
-        clean_filename = clean_filename.replace('-', '_')
+            " ", "_"
+        )  # Replace spaces with underscores
+        clean_filename = sanitized_title.replace("__", "_").replace(".", "")
+        clean_filename = clean_filename.replace("-", "_")
 
         return clean_filename.lower()
 
@@ -80,8 +80,7 @@ class TwspaceDL:
     def playlist_text(self) -> str:
         """Modify the chunks URL using the master one to be able to download"""
         playlist_text = API.client.get(self.playlist_url).text
-        master_url_wo_file = re.sub(
-            r"master_playlist\.m3u8.*", "", self.master_url)
+        master_url_wo_file = re.sub(r"master_playlist\.m3u8.*", "", self.master_url)
         playlist_text = re.sub(r"(?=chunk)", master_url_wo_file, playlist_text)
         return playlist_text
 
@@ -188,11 +187,9 @@ class TwspaceDL:
                 ]
                 meta.save()
             else:
-                logging.error(
-                    f"Unsupported user profile image format: {cover_ext}")
+                logging.error(f"Unsupported user profile image format: {cover_ext}")
         except RuntimeError:
-            logging.error(
-                f"Cannot download user profile image from URL: {cover_url}")
+            logging.error(f"Cannot download user profile image from URL: {cover_url}")
             raise
 
     def cleanup(self) -> None:
